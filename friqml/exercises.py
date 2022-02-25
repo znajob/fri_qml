613df4f535ed52ac6c6a141974e0974f69c49844
znajob <znajob@gmail.com>
2022-02-25 14:16:57 +0100

    fix exercise local variables

--- friqml/exercises.py
+++ friqml/exercises.py
@@ -3,13 +3,13 @@ from pennylane import numpy as np
 
 
 # EXERCISE 5
+e5_THETA = np.pi/4
+e5_PHI = np.pi/3
 def e5_hidden_preparation(wires=0):
-    THETA = np.pi/4
-    PHI = np.pi/3
-    qml.RY(THETA, wires=0)
-    qml.RZ(PHI, wires=0)
+    qml.RY(e5_THETA, wires=0)
+    qml.RZ(e5_PHI, wires=0)
 
 
 def e5_check_hidden_state(psi):
-    psi0 = [np.cos(THETA/2), np.exp(-1j*PHI)*np.sin(THETA/2)]
+    psi0 = [np.cos(e5_THETA/2), np.exp(-1j*e5_PHI)*np.sin(e5_THETA/2)]
     return eps > np.abs(1-np.abs(np.dot(np.conj(psi0), psi)))
