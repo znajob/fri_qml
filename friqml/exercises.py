680e7e4250900a7f63ffa0eef7c7e190d70e28cb
znajob <znajob@gmail.com>
2022-02-25 14:11:44 +0100

    add solutions

--- friqml/exercises.py
+++ friqml/exercises.py
@@ -1,12 +1,15 @@
 import pennylane as qml
 from pennylane import numpy as np
 
-THETA = np.pi/4
-PHI = np.pi/3
-def hidden_preparation(wires=0):
+
+# EXERCISE 5
+def e5_hidden_preparation(wires=0):
+    THETA = np.pi/4
+    PHI = np.pi/3
     qml.RY(THETA, wires=0)
     qml.RZ(PHI, wires=0)
 
-def check_hidden_state(psi):
-    psi0=[np.cos(THETA/2),np.exp(-1j*PHI)*np.sin(THETA/2)]
-    return eps>np.abs(1-np.abs(np.dot(np.conj(psi0),psi)))
+
+def e5_check_hidden_state(psi):
+    psi0 = [np.cos(THETA/2), np.exp(-1j*PHI)*np.sin(THETA/2)]
+    return eps > np.abs(1-np.abs(np.dot(np.conj(psi0), psi)))
