791ef73074eedbde123484b241a73f5f4eebd6c1
znajob <znajob@gmail.com>
2022-02-25 13:47:00 +0100

    remove prints

--- friqml/exercises.py
+++ friqml/exercises.py
@@ -8,8 +8,5 @@ def hidden_preparation(wires=0):
     qml.RZ(PHI, wires=0)
 
 def check_hidden_state(psi):
-    print(PHI/np.pi,THETA/np.pi)
     psi0=[np.cos(THETA/2),np.exp(-1j*PHI)*np.sin(THETA/2)]
-    print(psi0)
-    print(psi)
     return eps>np.abs(1-np.abs(np.dot(np.conj(psi0),psi)))
