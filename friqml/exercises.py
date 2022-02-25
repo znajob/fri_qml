1cbd9df82378c6d4be69ecacf3603e8ead3fc41e
znajob <znajob@gmail.com>
2022-02-25 12:09:51 +0100

    fixes

--- friqml/exercises.py
+++ friqml/exercises.py
@@ -2,17 +2,6 @@ import pennylane as qml
 from pennylane import numpy as np
 
 
-def random_state_unnormalized(n=4):
-    x = np.random.rand()+1j*np.random.rand()
-    return x
-
-
-def random_state_normalized(n=4):
-    x = random_state_unnormalized(n)
-    x /= np.linalg.norm(x)
-    return x
-
-
 def single_qubit_device():
     return qml.device('default.qubit', wires=1, shots=None)
 
--- friqml/utils.py
+++ friqml/utils.py
@@ -1,1 +1,15 @@
+import pennylane as qml
+from pennylane import numpy as np
+
 eps = 1e-13
+
+
+def random_state_unnormalized(n=4):
+    x = np.random.rand()+1j*np.random.rand()
+    return x
+
+
+def random_state_normalized(n=4):
+    x = random_state_unnormalized(n)
+    x /= np.linalg.norm(x)
+    return x
