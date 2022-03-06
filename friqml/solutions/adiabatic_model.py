fd2abf79559c76d9b342a7750bbe0479b9340da4
znajob <znajob@gmail.com>
2022-03-06 21:30:53 +0100

    small_gap_model

--- friqml/solutions/adiabatic_model.py
+++ friqml/solutions/adiabatic_model.py
@@ -6,11 +6,23 @@ from friqml.utils import eps, sz, sx
 
 # EXERCISE 1
 def small_gap_hamiltonian():
-  H = -1000*sz(0,3)@sz(1,3) - 0.1*sz(1,3)@sz(2,3) - 0.5*sz(0,3)
-  return H
+    H = -1000*sz(0, 3)@sz(1, 3) - 0.1*sz(1, 3)@sz(2, 3) - 0.5*sz(0, 3)
+    return H
+
 
 def big_gap_hamiltonian():
-  H = -sx(0,3)-sx(1,3)-sx(2,3)
-  return H
+    H = -sx(0, 3)-sx(1, 3)-sx(2, 3)
+    return H
 
-  
+
+# EXERCISE 2
+def small_gap_model():
+    a = np.zeros(3)
+    a[0] = -0.5
+
+    b = np.zeros([3, 3])
+    b[0, 1] = -1000
+    b[1, 2] = -0.1
+
+    qbm = dimod.BinaryQuadraticModel(a, b, "SPIN")
+    return qbm
