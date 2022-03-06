62c76b130a6db47a5e6d9acdb6515abd4c8535f8
znajob <znajob@gmail.com>
2022-03-06 21:09:56 +0100

    fix

--- friqml/solutions/adiabatic_model.py
+++ friqml/solutions/adiabatic_model.py
@@ -6,7 +6,7 @@ from friqml.utils import eps, sz, sx
 
 # EXERCISE 1
 def small_gap_hamiltonian():
-  H = -1000*sz(0,3)*sz(1,3) - 0.01*sz(1,3)*sz(2,3) - 0.5*sz(0,3)
+  H = -1000*sz(0,3)*sz(1,3) - 0.1*sz(1,3)*sz(2,3) - 0.5*sz(0,3)
   return H
 
 def big_gap_hamiltonian():
