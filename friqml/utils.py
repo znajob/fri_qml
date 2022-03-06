3df16e65d044cd9e3dc1aaf4b2e4be1eb4c9e193
znajob <znajob@gmail.com>
2022-03-06 17:07:51 +0100

    refractor

--- friqml/utils.py
+++ friqml/utils.py
@@ -13,3 +13,47 @@ def random_state_normalized(n=4):
     x = random_state_unnormalized(n)
     x /= np.linalg.norm(x)
     return x
+
+
+def get_vector(alpha, beta):
+    """
+    Function to compute 3D Cartesian coordinates
+    from 2D qubit vector.
+    """
+
+    # get phases
+    angle_alpha = cmath.phase(alpha)
+    angle_beta = cmath.phase(beta)
+
+    # avoiding wrong normalization due to rounding errors
+    if cmath.isclose(angle_alpha, cmath.pi):
+        angle_alpha = 0
+    if cmath.isclose(angle_beta, cmath.pi):
+        angle_beta = 0
+
+    if (angle_beta < 0 and angle_alpha < angle_beta) or (angle_beta > 0 and angle_alpha > angle_beta):
+        denominator = cmath.exp(1j*angle_beta)
+    else:
+        denominator = cmath.exp(1j*angle_alpha)
+
+    # eliminate global phase
+    alpha_new = alpha/denominator
+    beta_new = beta/denominator
+
+    # special case to avoid division by zero
+    if abs(alpha) == 0 or abs(beta) == 0:
+        if alpha == 0:
+            return [0, 0, -1]
+        else:
+            return [0, 0, 1]
+    else:
+        # compute theta and phi from alpha and beta
+        theta = 2*cmath.acos(alpha_new)
+        phi = -1j*cmath.log(beta_new/cmath.sin(theta/2))
+
+        # compute the Cartesian coordinates
+        x = cmath.sin(theta)*cmath.cos(phi)
+        y = cmath.sin(theta)*cmath.sin(phi)
+        z = cmath.cos(theta)
+
+    return [x.real, y.real, z.real]
--- friqml/visualisation.py
+++ friqml/visualisation.py
@@ -2,49 +2,7 @@ import cmath
 from qutip import Bloch
 import matplotlib.pyplot as plt
 from pennylane import numpy as np
-
-def get_vector(alpha, beta):
-    """
-    Function to compute 3D Cartesian coordinates
-    from 2D qubit vector.
-    """
-
-    # get phases
-    angle_alpha = cmath.phase(alpha)
-    angle_beta = cmath.phase(beta)
-
-    # avoiding wrong normalization due to rounding errors
-    if cmath.isclose(angle_alpha, cmath.pi):
-        angle_alpha = 0
-    if cmath.isclose(angle_beta, cmath.pi):
-        angle_beta = 0
-
-    if (angle_beta < 0 and angle_alpha < angle_beta) or (angle_beta > 0 and angle_alpha > angle_beta):
-        denominator = cmath.exp(1j*angle_beta)
-    else:
-        denominator = cmath.exp(1j*angle_alpha)
-
-    # eliminate global phase
-    alpha_new = alpha/denominator
-    beta_new = beta/denominator
-
-    # special case to avoid division by zero
-    if abs(alpha) == 0 or abs(beta) == 0:
-        if alpha == 0:
-            return [0, 0, -1]
-        else:
-            return [0, 0, 1]
-    else:
-        # compute theta and phi from alpha and beta
-        theta = 2*cmath.acos(alpha_new)
-        phi = -1j*cmath.log(beta_new/cmath.sin(theta/2))
-
-        # compute the Cartesian coordinates
-        x = cmath.sin(theta)*cmath.cos(phi)
-        y = cmath.sin(theta)*cmath.sin(phi)
-        z = cmath.cos(theta)
-
-    return [x.real, y.real, z.real]
+from friqml.utils import get_vector
 
 
 def plot_quantum_state(amplitudes):
