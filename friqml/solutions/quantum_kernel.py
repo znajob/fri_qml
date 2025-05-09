7ec0fc4590364cc562a5fa6047eef1e00157712d
znajob <znajob@gmail.com>
2025-05-09 11:57:51 +0200

    controlled unitary upudate

--- friqml/solutions/quantum_kernel.py
+++ friqml/solutions/quantum_kernel.py
@@ -10,21 +10,26 @@ def prepare_ancilla():
 
 
 def prepare_example_1():
-    qml.ControlledQubitUnitary([[0, 1], [1, 0]], control_wires=[
-                               0, 1], wires=2, control_values="10")
+    # qml.ControlledQubitUnitary([[0, 1], [1, 0]], control_wires=[
+    #                            0, 1], wires=2, control_values="10")
+    qml.ControlledQubitUnitary([[0, 1], [1, 0]], wires=[0,1,2], control_values="10")
 
 
 def prepare_example_2():
     fi = np.pi/4
+    # qml.ControlledQubitUnitary([[np.cos(fi), -np.sin(fi)], [np.sin(fi), np.cos(fi)]],
+    #                            control_wires=[0, 1], wires=2, control_values="11")
     qml.ControlledQubitUnitary([[np.cos(fi), -np.sin(fi)], [np.sin(fi), np.cos(fi)]],
-                               control_wires=[0, 1], wires=2, control_values="11")
+                               wires=[0,1,2], control_values="11")
 
 
 def prepare_class():
-    qml.ControlledQubitUnitary([[0, 1], [1, 0]], control_wires=[
-                               0, 1], wires=3, control_values="11")
-    qml.ControlledQubitUnitary([[0, 1], [1, 0]], control_wires=[
-                               0, 1], wires=3, control_values="01")
+    # qml.ControlledQubitUnitary([[0, 1], [1, 0]], control_wires=[
+    #                            0, 1], wires=3, control_values="11")
+    # qml.ControlledQubitUnitary([[0, 1], [1, 0]], control_wires=[
+    #                            0, 1], wires=3, control_values="01")
+    qml.ControlledQubitUnitary([[0, 1], [1, 0]], wires=[0,1,3], control_values="11")
+    qml.ControlledQubitUnitary([[0, 1], [1, 0]], wires=[0,1,3], control_values="01")
 
 
 def state_preparation():
