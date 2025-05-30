01100e80df9fbc58aa81eba6a691c4c7132d0030
znajob <znajob@gmail.com>
2025-05-30 12:56:00 +0200

    order of exercises fix

--- friqml/solutions/tensor_network_compression.py
+++ friqml/solutions/tensor_network_compression.py
@@ -20,6 +20,24 @@ transpose_and_reshape = _transpose_and_reshape
 
 
 # EXERCISE 2
+def _inverse_transpose_and_reshape(mps, nbit):
+    """Restores the image from the MPS format."""
+    idims = [2*i for i in range(nbit)]+[2*i+1 for i in range(nbit)]
+    data_c = mps[0]
+    for i in range(1, nbit):
+        data_c = np.einsum("...i,ijk->...jk", data_c, mps[i])
+    data_c = np.reshape(data_c, [2]*(2*nbit))
+    data_c = np.transpose(data_c, idims)
+    size = 2**nbit
+    img_c = np.reshape(data_c, [size, size])
+    return img_c
+
+
+inverse_transpose_and_reshape = _inverse_transpose_and_reshape
+
+
+
+# EXERCISE 3
 def _mps_compress(A, nbit, Dmax):
     """Compresses the tensor using MPS with a given maximum bond dimension."""
     mps = []
@@ -40,24 +58,6 @@ def _mps_compress(A, nbit, Dmax):
 
 
 mps_compress = _mps_compress
-
-# EXERCISE 3
-
-
-def _inverse_transpose_and_reshape(mps, nbit):
-    """Restores the image from the MPS format."""
-    idims = [2*i for i in range(nbit)]+[2*i+1 for i in range(nbit)]
-    data_c = mps[0]
-    for i in range(1, nbit):
-        data_c = np.einsum("...i,ijk->...jk", data_c, mps[i])
-    data_c = np.reshape(data_c, [2]*(2*nbit))
-    data_c = np.transpose(data_c, idims)
-    size = 2**nbit
-    img_c = np.reshape(data_c, [size, size])
-    return img_c
-
-
-inverse_transpose_and_reshape = _inverse_transpose_and_reshape
 
 
 # EXERCISE 4
